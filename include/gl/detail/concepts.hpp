536379d3c3de8eed8dcd85d2258599d337dd8de4
SpectraL519 <jmusial2003@gmail.com>
2024-07-17 23:50:58 +0200

    added vertex_descriptor class tests

--- include/gl/detail/concepts.hpp
+++ include/gl/detail/concepts.hpp
@@ -6,6 +6,7 @@ namespace gl::detail {
 
 template <typename T>
 concept c_properties =
-    std::default_initializable<T> and std::copy_constructible<T> and std::move_constructible<T>;
+    std::default_initializable<T> and std::copy_constructible<T> and std::move_constructible<T>
+    and std::assignable_from<T&, const T&>;
 
 } // namespace gl::detail
--- include/gl/vertex_descriptor.hpp
+++ include/gl/vertex_descriptor.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
 #include "detail/concepts.hpp"
+#include "detail/default_types.hpp"
 
 namespace gl {
 
-template <detail::c_properties Properties>
+/*
+TODO: define the properties member only when Properties is not void
+Possible solutions:
+- Template specialization
+- std::enable_if_t ?
+Requires:
+- Modification of the c_properties concept to be a valid type or void
+*/
+
+template <detail::c_properties Properties = detail::empty_properties>
 class vertex_descriptor {
 public:
     using properties_type = Properties;
@@ -14,7 +24,8 @@ public:
     vertex_descriptor(const std::size_t id) : _id(id) {}
 
     vertex_descriptor(const std::size_t id, const properties_type& properties)
-    : _list_id(id), _properties(properties) {}
+    requires(not std::is_same_v<properties_type, detail::empty_properties>)
+    : _id(id), properties(properties) {}
 
     vertex_descriptor(const vertex_descriptor&) = default;
     vertex_descriptor(vertex_descriptor&&) = default;
