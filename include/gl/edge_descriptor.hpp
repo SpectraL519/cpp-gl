b422e5eeba7eeb21631c5f0c0eed70b912008953
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2024-07-26 19:02:38 +0200

    YT-CPPGL-5: Create the edge class
    Added:
    - the `edge_descriptor` class with template aliases: `edge`, `directed_edge` and `undirected_edge`
    - the `directed_t` and `undirected_t` edge tags with the `c_edge_directional_tag` concept
    - the general concepts: `c_instantiation_of` and `c_one_of`
    - unit tests covering the added functionality

---
+++ include/gl/edge_descriptor.hpp
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "detail/concepts.hpp"
+#include "detail/default_types.hpp"
+#include "edge_tags.hpp"
+#include "vertex_descriptor.hpp"
+
+#include <memory>
+#include <type_traits>
+#include <vector>
+
+namespace gl {
+
+template <
+    detail::c_instantiation_of<vertex_descriptor> VertexType,
+    detail::c_edge_directional_tag DirectionalTag = directed_t,
+    detail::c_properties Properties = detail::empty_properties>
+class edge_descriptor {
+public:
+    using type = edge_descriptor<VertexType, DirectionalTag, Properties>;
+    using vertex_type = std::shared_ptr<VertexType>;
+    using directional_tag = DirectionalTag;
+    using properties_type = Properties;
+
+    friend directional_tag;
+
+    edge_descriptor() = delete;
+
+    explicit edge_descriptor(vertex_type u, vertex_type v) : _vertices(u, v) {}
+
+    explicit edge_descriptor(vertex_type u, vertex_type v, const properties_type& properties)
+    requires(not std::is_same_v<properties_type, detail::empty_properties>)
+    : _vertices(u, v), properties(properties) {}
+
+    edge_descriptor(const edge_descriptor&) = default;
+    edge_descriptor(edge_descriptor&&) = default;
+
+    edge_descriptor& operator=(const edge_descriptor&) = default;
+    edge_descriptor& operator=(edge_descriptor&&) = default;
+
+    ~edge_descriptor() = default;
+
+    [[nodiscard]] inline constexpr bool is_directed() const {
+        return std::is_same_v<directional_tag, directed_t>;
+    }
+
+    [[nodiscard]] inline constexpr bool is_undirected() const {
+        return std::is_same_v<directional_tag, undirected_t>;
+    }
+
+    [[nodiscard]] inline const std::pair<vertex_type, vertex_type>& incident_vertices() const {
+        return this->_vertices;
+    }
+
+    [[nodiscard]] inline vertex_type first() const {
+        return this->_vertices.first;
+    }
+
+    [[nodiscard]] inline vertex_type second() const {
+        return this->_vertices.second;
+    }
+
+    [[nodiscard]] vertex_type incident_vertex(const vertex_type& vertex) const {
+        if (*vertex == *this->_vertices.first)
+            return this->_vertices.second;
+
+        if (*vertex == *this->_vertices.second)
+            return this->_vertices.first;
+
+        return nullptr;
+    }
+
+    [[nodiscard]] inline bool is_incident_from(const vertex_type& vertex) const {
+        return directional_tag::is_incident_from(*this, vertex);
+    }
+
+    [[nodiscard]] inline bool is_incident_to(const vertex_type& vertex) const {
+        return directional_tag::is_incident_to(*this, vertex);
+    }
+
+    properties_type properties = {};
+
+private:
+    std::pair<vertex_type, vertex_type> _vertices;
+};
+
+template <
+    detail::c_instantiation_of<vertex_descriptor> VertexType,
+    detail::c_edge_directional_tag DirectionalTag = directed_t,
+    detail::c_properties Properties = detail::empty_properties>
+using edge = edge_descriptor<VertexType, DirectionalTag, Properties>;
+
+template <
+    detail::c_instantiation_of<vertex_descriptor> VertexType,
+    detail::c_properties Properties = detail::empty_properties>
+using directed_edge = edge_descriptor<VertexType, directed_t, Properties>;
+
+template <
+    detail::c_instantiation_of<vertex_descriptor> VertexType,
+    detail::c_properties Properties = detail::empty_properties>
+using undirected_edge = edge_descriptor<VertexType, undirected_t, Properties>;
+
+} // namespace gl
---
+++ tests/source/test_edge_descriptor.cpp
@@ -0,0 +1,126 @@
+#include <doctest.h>
+#include <gl/edge_descriptor.hpp>
+
+using namespace gl;
+
+namespace {
+
+constexpr std::size_t vertex_id_1 = 1111ull;
+constexpr std::size_t vertex_id_2 = 2222ull;
+constexpr std::size_t vertex_id_3 = 3333ull;
+
+struct used_property {
+    bool operator==(const used_property&) const = default;
+    bool used;
+};
+
+} // namespace
+
+namespace gl_testing {
+
+TEST_SUITE_BEGIN("test_edge_descriptor");
+
+struct test_edge_descriptor {
+    std::shared_ptr<vertex<>> vd_1 = std::make_shared<vertex<>>(vertex_id_1);
+    std::shared_ptr<vertex<>> vd_2 = std::make_shared<vertex<>>(vertex_id_2);
+    std::shared_ptr<vertex<>> vd_3 = std::make_shared<vertex<>>(vertex_id_3);
+};
+
+TEST_CASE_FIXTURE(
+    test_edge_descriptor, "is_directed() should return true only for edges with directed edge tag"
+) {
+    directed_edge<vertex<>> directed_edge{vd_1, vd_2};
+    CHECK(directed_edge.is_directed());
+
+    undirected_edge<vertex<>> bidirectional_edge{vd_1, vd_2};
+    CHECK_FALSE(bidirectional_edge.is_directed());
+}
+
+TEST_CASE_FIXTURE(
+    test_edge_descriptor,
+    "is_undirected() should return true only for edges with bidirectional edge tag"
+) {
+    undirected_edge<vertex<>> bidirectional_edge{vd_1, vd_2};
+    CHECK(bidirectional_edge.is_undirected());
+
+    directed_edge<vertex<>> directed_edge{vd_1, vd_2};
+    CHECK_FALSE(directed_edge.is_undirected());
+}
+
+TEST_CASE_TEMPLATE_DEFINE(
+    "properties should be properly initialized", EdgeType, properties_edge_directional_tag_template
+) {
+    test_edge_descriptor fixture;
+
+    const used_property used{true};
+    const EdgeType sut{fixture.vd_1, fixture.vd_2, used};
+
+    CHECK_EQ(sut.properties, used);
+}
+
+TEST_CASE_TEMPLATE_INSTANTIATE(properties_edge_directional_tag_template, directed_edge<vertex<>, used_property>, undirected_edge<vertex<>, used_property>);
+
+TEST_CASE_TEMPLATE_DEFINE(
+    "directional_tag-independent tests", EdgeType, edge_directional_tag_template
+) {
+    test_edge_descriptor fixture{};
+
+    EdgeType sut{fixture.vd_1, fixture.vd_2};
+
+    SUBCASE("incident_vertices should return the pair of vertices the edge was initialized with") {
+        const auto& vertices = sut.incident_vertices();
+        CHECK_EQ(*vertices.first, *fixture.vd_1);
+        CHECK_EQ(*vertices.second, *fixture.vd_2);
+    }
+
+    SUBCASE("first should return the first vertex descriptor the edge was initialized with") {
+        CHECK_EQ(*sut.first(), *fixture.vd_1);
+    }
+
+    SUBCASE("second should return the second vertex descriptor the edge was initialized with") {
+        CHECK_EQ(*sut.second(), *fixture.vd_2);
+    }
+
+    SUBCASE("incident_vertex should return nullptr if input vertex is not incident with the edge") {
+        CHECK_FALSE(sut.incident_vertex(fixture.vd_3));
+    }
+
+    SUBCASE("incident_vertex should return the vertex incident with the input vertex") {
+        CHECK_EQ(*sut.incident_vertex(fixture.vd_1), *fixture.vd_2);
+        CHECK_EQ(*sut.incident_vertex(fixture.vd_2), *fixture.vd_1);
+    }
+}
+
+TEST_CASE_TEMPLATE_INSTANTIATE(edge_directional_tag_template, directed_edge<vertex<>>, undirected_edge<vertex<>>);
+
+TEST_CASE_FIXTURE(test_edge_descriptor, "is_incident_from tests") {
+    SUBCASE("[undirected_edge] should return true for both vertices") {
+        undirected_edge<vertex<>> sut{vd_1, vd_2};
+        CHECK(sut.is_incident_from(vd_1));
+        CHECK(sut.is_incident_from(vd_2));
+    }
+
+    SUBCASE("[directed_edge] should return true only for the first vertex") {
+        directed_edge<vertex<>> sut{vd_1, vd_2};
+        CHECK(sut.is_incident_from(vd_1));
+        CHECK_FALSE(sut.is_incident_from(vd_2));
+    }
+}
+
+TEST_CASE_FIXTURE(test_edge_descriptor, "is_incident_to tests") {
+    SUBCASE("[undirected_edge] should return true for both vertices") {
+        undirected_edge<vertex<>> sut{vd_1, vd_2};
+        CHECK(sut.is_incident_to(vd_1));
+        CHECK(sut.is_incident_to(vd_2));
+    }
+
+    SUBCASE("[directed_edge] should return true only for the second vertex") {
+        directed_edge<vertex<>> sut{vd_1, vd_2};
+        CHECK(sut.is_incident_to(vd_2));
+        CHECK_FALSE(sut.is_incident_to(vd_1));
+    }
+}
+
+TEST_SUITE_END(); // test_edge_descriptor
+
+} // namespace gl_testing
