bb403c3adc0dd28e5a2196fd0a2aa715e1faef35
SpectraL519 <jmusial2003@gmail.com>
2024-08-24 23:07:20 +0200

    add_edge(edge, edge) method

--- include/gl/incidence.hpp
+++ include/gl/incidence.hpp
@@ -5,6 +5,9 @@
 
 namespace gl {
 
+// TODO: modify these methods to take the graph as parameter
+// requires adding a has_vertex(vertex_ptr) method to the graph class
+
 template <type_traits::c_instantiation_of<graph> GraphType>
 [[nodiscard]] gl_attr_force_inline bool are_incident(
     const typename GraphType::vertex_ptr_type& vertex, const typename GraphType::edge_ptr_type& edge
@@ -17,6 +20,13 @@ template <type_traits::c_instantiation_of<graph> GraphType>
     const typename GraphType::edge_ptr_type& edge, const typename GraphType::vertex_ptr_type& vertex
 ) {
     return edge->is_incident_with(vertex);
+}
+
+template <type_traits::c_instantiation_of<graph> GraphType>
+[[nodiscard]] gl_attr_force_inline bool are_incident(
+    const typename GraphType::edge_ptr_type& edge_1, const typename GraphType::edge_ptr_type& edge_2
+) {
+    return edge_1->is_incident_with(edge_2->first()) or edge_1->is_incident_with(edge_2->second());
 }
 
 } // namespace gl
--- tests/source/test_incidence.cpp
+++ tests/source/test_incidence.cpp
@@ -13,6 +13,7 @@ struct test_incidence {
 
     std::shared_ptr<vertex_type> vd_1 = std::make_shared<vertex_type>(constants::vertex_id_1);
     std::shared_ptr<vertex_type> vd_2 = std::make_shared<vertex_type>(constants::vertex_id_2);
+    std::shared_ptr<vertex_type> vd_3 = std::make_shared<vertex_type>(constants::vertex_id_3);
 
     std::shared_ptr<vertex_type> invalid_vd_1 =
         std::make_shared<vertex_type>(constants::vertex_id_1);
@@ -20,25 +21,47 @@ struct test_incidence {
         std::make_shared<vertex_type>(constants::vertex_id_2);
 };
 
-TEST_CASE_TEMPLATE_DEFINE(
-    "are_incident should return true only when an edge and a vertex are incident",
-    GraphType,
-    graph_type_template
-) {
+TEST_CASE_TEMPLATE_DEFINE("incidence functions tests", GraphType, graph_type_template) {
     test_incidence fixture;
-    const auto edge = lib::make_edge<typename GraphType::edge_type>(fixture.vd_1, fixture.vd_2);
 
-    CHECK(lib::are_incident<GraphType>(fixture.vd_1, edge));
-    CHECK(lib::are_incident<GraphType>(fixture.vd_2, edge));
+    SUBCASE("are_incident(vertex and edge pair) should return true only when the edge and the "
+            "vertex are incident with each other") {
+        const auto edge = lib::make_edge<typename GraphType::edge_type>(fixture.vd_1, fixture.vd_2);
 
-    CHECK(lib::are_incident<GraphType>(edge, fixture.vd_1));
-    CHECK(lib::are_incident<GraphType>(edge, fixture.vd_2));
+        CHECK(lib::are_incident<GraphType>(fixture.vd_1, edge));
+        CHECK(lib::are_incident<GraphType>(fixture.vd_2, edge));
 
-    CHECK_FALSE(lib::are_incident<GraphType>(fixture.invalid_vd_1, edge));
-    CHECK_FALSE(lib::are_incident<GraphType>(fixture.invalid_vd_2, edge));
+        CHECK(lib::are_incident<GraphType>(edge, fixture.vd_1));
+        CHECK(lib::are_incident<GraphType>(edge, fixture.vd_2));
 
-    CHECK_FALSE(lib::are_incident<GraphType>(edge, fixture.invalid_vd_1));
-    CHECK_FALSE(lib::are_incident<GraphType>(edge, fixture.invalid_vd_2));
+        CHECK_FALSE(lib::are_incident<GraphType>(fixture.invalid_vd_1, edge));
+        CHECK_FALSE(lib::are_incident<GraphType>(fixture.invalid_vd_2, edge));
+
+        CHECK_FALSE(lib::are_incident<GraphType>(edge, fixture.invalid_vd_1));
+        CHECK_FALSE(lib::are_incident<GraphType>(edge, fixture.invalid_vd_2));
+    }
+
+    SUBCASE("are_incident(edge, edge) should return true only when the edges share a common vertex"
+    ) {
+        const auto edge_1 =
+            lib::make_edge<typename GraphType::edge_type>(fixture.vd_1, fixture.vd_2);
+        const auto edge_2 =
+            lib::make_edge<typename GraphType::edge_type>(fixture.vd_2, fixture.vd_3);
+        const auto edge_3 =
+            lib::make_edge<typename GraphType::edge_type>(fixture.vd_3, fixture.vd_1);
+
+        CHECK(lib::are_incident<GraphType>(edge_1, edge_2));
+        CHECK(lib::are_incident<GraphType>(edge_2, edge_3));
+        CHECK(lib::are_incident<GraphType>(edge_3, edge_1));
+
+        const auto invalid_edge = lib::make_edge<typename GraphType::edge_type>(
+            fixture.invalid_vd_1, fixture.invalid_vd_2
+        );
+
+        CHECK_FALSE(lib::are_incident<GraphType>(edge_1, invalid_edge));
+        CHECK_FALSE(lib::are_incident<GraphType>(edge_2, invalid_edge));
+        CHECK_FALSE(lib::are_incident<GraphType>(edge_2, invalid_edge));
+    }
 }
 
 TEST_CASE_TEMPLATE_INSTANTIATE(
