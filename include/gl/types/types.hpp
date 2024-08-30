e06dca01f7eda9530c4338db6e24abd9d5a127c3
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2024-08-30 15:53:12 +0200

    YT-CPPGL-33: Extend the edge getting functionality
    Added the `get_edge` and `get_edges` methods to the `graph` class allowing for extracting a single all edges connecting the given vertices depending on the graph implementation

--- include/gl/types/types.hpp
+++ include/gl/types/types.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <optional>
 #include <utility>
 
 namespace gl::types {
@@ -10,5 +11,8 @@ using size_type = std::uint64_t;
 
 template <typename T>
 using homogeneous_pair = std::pair<T, T>;
+
+template <typename T>
+using optional_ref = std::optional<std::reference_wrapper<std::remove_reference_t<T>>>;
 
 } // namespace gl::types
