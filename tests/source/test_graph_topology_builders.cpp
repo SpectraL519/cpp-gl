8dd23f23d8749fff3a4af6e6188255a795cff4d8
SpectraL519 <jmusial2003@gmail.com>
2024-09-01 11:56:22 +0200

    path graph builders

--- tests/source/test_graph_topology_builders.cpp
+++ tests/source/test_graph_topology_builders.cpp
@@ -22,6 +22,16 @@ template <lib_tt::c_graph_type GraphType>
 }
 
 template <lib_tt::c_graph_type GraphType>
+[[nodiscard]] auto is_vertex_not_connected(const GraphType& graph) {
+    using vertex_type = typename GraphType::vertex_type;
+    return [&graph](const vertex_type& source_vertex) {
+        return std::ranges::none_of(graph.vertices(), [&](const vertex_type& destination_vertex) {
+            return graph.has_edge(source_vertex, destination_vertex);
+        });
+    };
+}
+
+template <lib_tt::c_graph_type GraphType>
 [[nodiscard]] auto is_vertex_connected_to_next_only(const GraphType& graph) {
     using vertex_type = typename GraphType::vertex_type;
     return [&graph](const vertex_type& source_vertex) {
@@ -35,19 +45,33 @@ template <lib_tt::c_graph_type GraphType>
 }
 
 template <lib_tt::c_graph_type GraphType>
+[[nodiscard]] auto is_vertex_connected_to_prev_only(const GraphType& graph) {
+    using vertex_type = typename GraphType::vertex_type;
+    return [&graph](const vertex_type& source_vertex) {
+        const auto& prev_vertex = graph.get_vertex(
+            (source_vertex.id() + graph.n_vertices() - constants::one_element) % graph.n_vertices()
+        );
+
+        return std::ranges::all_of(graph.vertices(), [&](const auto& vertex) {
+            return (vertex == prev_vertex) == graph.has_edge(source_vertex, vertex);
+        });
+    };
+}
+
+template <lib_tt::c_graph_type GraphType>
 [[nodiscard]] auto is_vertex_connected_to_id_adjacent(const GraphType& graph) {
     using vertex_type = typename GraphType::vertex_type;
     return [&graph](const vertex_type& source_vertex) {
         const auto& next_vertex =
             graph.get_vertex((source_vertex.id() + constants::one_element) % graph.n_vertices());
 
-        const auto& prev_vertex =
-            graph.get_vertex(
-                (source_vertex.id() + graph.n_vertices() - constants::one_element) % graph.n_vertices()
-            );
+        const auto& prev_vertex = graph.get_vertex(
+            (source_vertex.id() + graph.n_vertices() - constants::one_element) % graph.n_vertices()
+        );
 
         return std::ranges::all_of(graph.vertices(), [&](const auto& vertex) {
-            return (vertex == prev_vertex or vertex == next_vertex) == graph.has_edge(source_vertex, vertex);
+            return (vertex == prev_vertex or vertex == next_vertex)
+                == graph.has_edge(source_vertex, vertex);
         });
     };
 }
@@ -107,6 +131,35 @@ TEST_CASE_TEMPLATE_DEFINE(
             cycle.vertices(), predicate::is_vertex_connected_to_id_adjacent(cycle)
         ));
     }
+
+    SUBCASE("path(n_vertices) should build a one-way path graph of size n_vertices") {
+        const auto path = lib::topology::path<graph_type>(constants::n_elements_top);
+        const auto n_source_vertices = path.n_vertices() - constants::one_element;
+
+        REQUIRE(std::ranges::all_of(
+            path.vertices() | std::views::take(n_source_vertices),
+            predicate::is_vertex_connected_to_next_only(path)
+        ));
+        CHECK(predicate::is_vertex_not_connected(path)(path.vertices().element_at(n_source_vertices)
+        ));
+    }
+
+    SUBCASE("bidirectional_path(n_vertices) should build a two-way path graph of size n_vertices") {
+        const auto path = lib::topology::bidirectional_path<graph_type>(constants::n_elements_top);
+        const auto n_source_vertices = path.n_vertices() - constants::one_element;
+
+        const auto vertices = path.vertices();
+
+        REQUIRE(std::ranges::all_of(
+            std::ranges::next(vertices.begin(), constants::one_element),
+            std::ranges::next(vertices.begin(), n_source_vertices),
+            predicate::is_vertex_connected_to_id_adjacent(path)
+        ));
+        CHECK(predicate::is_vertex_connected_to_next_only(path)(*path.vertices().begin()));
+        CHECK(predicate::is_vertex_connected_to_prev_only(path)(
+            path.vertices().element_at(n_source_vertices)
+        ));
+    }
 }
 
 TEST_CASE_TEMPLATE_INSTANTIATE(
@@ -126,6 +179,23 @@ TEST_CASE_TEMPLATE_DEFINE(
         const auto cycle = lib::topology::cycle<graph_type>(constants::n_elements_top);
         CHECK(std::ranges::all_of(
             cycle.vertices(), predicate::is_vertex_connected_to_id_adjacent(cycle)
+        ));
+    }
+
+    SUBCASE("path(n_vertices) should build a two-way path graph of size n_vertices") {
+        const auto path = lib::topology::path<graph_type>(constants::n_elements_top);
+        const auto n_source_vertices = path.n_vertices() - constants::one_element;
+
+        const auto vertices = path.vertices();
+
+        REQUIRE(std::ranges::all_of(
+            std::ranges::next(vertices.begin(), constants::one_element),
+            std::ranges::next(vertices.begin(), n_source_vertices),
+            predicate::is_vertex_connected_to_id_adjacent(path)
+        ));
+        CHECK(predicate::is_vertex_connected_to_next_only(path)(*path.vertices().begin()));
+        CHECK(predicate::is_vertex_connected_to_prev_only(path)(
+            path.vertices().element_at(n_source_vertices)
         ));
     }
 }
