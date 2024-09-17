ff372cde05a6f3f80c9ca3798eabe6363a1ea2e7
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2024-09-17 21:05:15 +0200

    YT-CPPGL-38: Add the decrement operator to custom iterators with bidirectional tag
    Added the the `operator--`, `operator--(int)` and `base() const` methods to both `dereferencing_iterator` and `non_null_iterator` classes

--- include/gl/types/non_null_iterator.hpp
+++ include/gl/types/non_null_iterator.hpp
@@ -22,9 +22,17 @@ public:
 
     non_null_iterator() = default;
 
-    non_null_iterator(iterator_type current, iterator_type end) : _current(current), _end(end) {
+    non_null_iterator(iterator_type current, iterator_type end)
+    requires(not std::bidirectional_iterator<iterator_type>)
+    : _begin(), _current(current), _end(end) {
         if (this->_current != this->_end and not *this->_current)
-            this->_skip_null_elements();
+            this->_skip_null_elements_forward();
+    }
+
+    non_null_iterator(iterator_type begin, iterator_type current, iterator_type end)
+    : _begin(begin), _current(current), _end(end) {
+        if (this->_current != this->_end and not *this->_current)
+            this->_skip_null_elements_forward();
     }
 
     non_null_iterator(const non_null_iterator&) = default;
@@ -42,13 +50,28 @@ public:
     }
 
     inline non_null_iterator& operator++() {
-        this->_skip_null_elements();
+        this->_skip_null_elements_forward();
         return *this;
     }
 
     inline non_null_iterator operator++(int) {
         non_null_iterator tmp = *this;
-        this->_skip_null_elements();
+        this->_skip_null_elements_forward();
+        return tmp;
+    }
+
+    inline non_null_iterator& operator--()
+    requires(std::bidirectional_iterator<iterator_type>)
+    {
+        this->_skip_null_elements_backward();
+        return *this;
+    }
+
+    inline non_null_iterator operator--(int)
+    requires(std::bidirectional_iterator<iterator_type>)
+    {
+        non_null_iterator tmp = *this;
+        this->_skip_null_elements_backward();
         return tmp;
     }
 
@@ -60,15 +83,48 @@ public:
         return this->_current != other._current;
     }
 
-private:
-    void _skip_null_elements() {
-        constexpr auto is_null = [](const reference& ptr) { return ptr == nullptr; };
+    [[nodiscard]] gl_attr_force_inline iterator_type base() const {
+        return this->_current;
+    }
 
+private:
+    static constexpr bool _is_null(const reference& ptr) {
+        return ptr == nullptr;
+    }
+
+    inline void _skip_null_elements_forward() {
         if (this->_current == this->_end)
             return;
 
-        this->_current = std::find_if_not(++this->_current, this->_end, is_null);
+        this->_current = std::find_if_not(++this->_current, this->_end, _is_null);
     }
+
+    void _skip_null_elements_backward()
+    requires(std::bidirectional_iterator<iterator_type>)
+    {
+        if (this->_current == this->_begin)
+            return;
+
+        this->_current =
+            std::find_if_not(
+                ++std::make_reverse_iterator(this->_current),
+                std::make_reverse_iterator(this->_begin),
+                _is_null
+            )
+                .base();
+
+        /*
+        reverse_iterator.base() points to an element one past the position
+        the iterator points to in a forward sequence so to make sure the new
+        current iterator is valid it needs to be adjusted by one backward
+        */
+        if (this->_current != this->_begin)
+            --this->_current;
+    }
+
+    [[no_unique_address]] std::
+        conditional_t<std::bidirectional_iterator<iterator_type>, iterator_type, std::monostate>
+            _begin;
 
     iterator_type _current;
     iterator_type _end;
@@ -77,23 +133,67 @@ private:
 } // namespace types
 
 template <type_traits::c_range Range>
-[[nodiscard]] gl_attr_force_inline auto non_null_begin(Range& range) {
+[[nodiscard]] auto non_null_begin(Range& range)
+requires(not std::bidirectional_iterator<decltype(std::ranges::begin(range))>)
+{
     return types::non_null_iterator(std::ranges::begin(range), std::ranges::end(range));
 }
 
 template <type_traits::c_range Range>
-[[nodiscard]] gl_attr_force_inline auto non_null_end(Range& range) {
+[[nodiscard]] auto non_null_end(Range& range)
+requires(not std::bidirectional_iterator<decltype(std::ranges::end(range))>)
+{
     return types::non_null_iterator(std::ranges::end(range), std::ranges::end(range));
 }
 
 template <type_traits::c_const_range Range>
-[[nodiscard]] gl_attr_force_inline auto non_null_cbegin(Range& range) {
+[[nodiscard]] auto non_null_cbegin(Range& range)
+requires(not std::bidirectional_iterator<decltype(std::ranges::cbegin(range))>)
+{
     return types::non_null_iterator(std::ranges::cbegin(range), std::ranges::cend(range));
 }
 
 template <type_traits::c_const_range Range>
-[[nodiscard]] gl_attr_force_inline auto non_null_cend(Range& range) {
+[[nodiscard]] auto non_null_cend(Range& range)
+requires(not std::bidirectional_iterator<decltype(std::ranges::cend(range))>)
+{
     return types::non_null_iterator(std::ranges::cend(range), std::ranges::cend(range));
+}
+
+template <type_traits::c_range Range>
+[[nodiscard]] auto non_null_begin(Range& range)
+requires(std::bidirectional_iterator<decltype(std::ranges::begin(range))>)
+{
+    return types::non_null_iterator(
+        std::ranges::begin(range), std::ranges::begin(range), std::ranges::end(range)
+    );
+}
+
+template <type_traits::c_range Range>
+[[nodiscard]] auto non_null_end(Range& range)
+requires(std::bidirectional_iterator<decltype(std::ranges::end(range))>)
+{
+    return types::non_null_iterator(
+        std::ranges::begin(range), std::ranges::end(range), std::ranges::end(range)
+    );
+}
+
+template <type_traits::c_const_range Range>
+[[nodiscard]] auto non_null_cbegin(Range& range)
+requires(std::bidirectional_iterator<decltype(std::ranges::cbegin(range))>)
+{
+    return types::non_null_iterator(
+        std::ranges::cbegin(range), std::ranges::cbegin(range), std::ranges::cend(range)
+    );
+}
+
+template <type_traits::c_const_range Range>
+[[nodiscard]] auto non_null_cend(Range& range)
+requires(std::bidirectional_iterator<decltype(std::ranges::cend(range))>)
+{
+    return types::non_null_iterator(
+        std::ranges::cbegin(range), std::ranges::cend(range), std::ranges::cend(range)
+    );
 }
 
 } // namespace gl
