144ac791a953f81f50fb0db1b3eab4eb23998c0f
SpectraL519 <jmusial2003@gmail.com>
2024-09-24 14:57:40 +0200

    init load

--- include/gl/graph_file_io.hpp
+++ include/gl/graph_file_io.hpp
@@ -25,7 +25,7 @@ requires(std::same_as<Mode, write>)
         );
 
     std::ofstream file;
-    file.open(path, std::ios_base::out | std::ios::trunc);
+    file.open(path, std::ios::trunc);
 
     if (not file.is_open())
         throw std::ios_base::failure(std::format("Could not open file `{}`", path.string()));
@@ -49,7 +49,31 @@ requires(std::same_as<Mode, append>)
         );
 
     std::ofstream file;
-    file.open(path, std::ios_base::out | std::ios::app);
+    file.open(path, std::ios::app);
+
+    if (not file.is_open())
+        throw std::ios_base::failure(std::format("Could not open file `{}`", path.string()));
+
+    return file;
+}
+
+[[nodiscard]] std::ifstream open_infile(const std::filesystem::path& path) {
+    if (not std::filesystem::exists(path))
+        throw std::filesystem::filesystem_error(
+            "Cannot load from file",
+            path,
+            std::make_error_code(std::errc::no_such_file_or_directory)
+        );
+
+    if (not std::filesystem::is_regular_file(path))
+        throw std::filesystem::filesystem_error(
+            "Cannot load from file (invalid file type - should be a regular file",
+            path,
+            std::make_error_code(std::errc::invalid_argument)
+        );
+
+    std::ifstream file;
+    file.open(path);
 
     if (not file.is_open())
         throw std::ios_base::failure(std::format("Could not open file `{}`", path.string()));
@@ -72,6 +96,18 @@ void save(
         file << option;
 
     file << graph;
+}
+
+template <type_traits::c_graph GraphType>
+[[nodiscard]] GraphType load(const std::filesystem::path& path = "graph.gsf") {
+    std::ifstream file = detail::open_infile(path);
+
+    file >> enable_gsf;
+
+    GraphType graph;
+    file >> graph;
+
+    return graph;
 }
 
 } // namespace gl::io
