66f1ab865370c840f5deba6577116370abaafb73
SpectraL519 <jmusial2003@gmail.com>
2024-09-24 10:40:14 +0200

    aligned tests

--- tests/source/test_stream_options_manipulator.cpp
+++ tests/source/test_stream_options_manipulator.cpp
@@ -12,54 +12,156 @@ TEST_SUITE_BEGIN("test_stream_options_manipulator");
 
 struct test_stream_options_manipulator {
     test_stream_options_manipulator() {
-        REQUIRE_FALSE(lib::io::is_option_set(ss1, property_bit_position));
-        REQUIRE_FALSE(lib::io::is_option_set(ss2, property_bit_position));
+        REQUIRE_FALSE(lib::io::is_option_set(ss1, bit_position_1));
+        REQUIRE_FALSE(lib::io::is_option_set(ss2, bit_position_1));
     }
 
     std::stringstream ss1;
     std::stringstream ss2;
 
-    const lib::io::bit_position_type property_bit_position = constants::first_element_idx;
+    static constexpr lib::io::bit_position_type bit_position_1 = constants::first_element_idx;
+    static constexpr lib::io::bit_position_type bit_position_2 = bit_position_1 + constants::one;
+    static constexpr lib::io::iword_type options_bitmask =
+        (lib::io::iword_bit << bit_position_1) | (lib::io::iword_bit << bit_position_2);
 };
 
 TEST_CASE_FIXTURE(
-    test_stream_options_manipulator, "stream_options_manipulator should properly handle istream property operations"
+    test_stream_options_manipulator,
+    "should properly handle istream option operations per stream"
 ) {
-    ss1 >> lib::io::set_option(property_bit_position);
-    CHECK(lib::io::is_option_set(ss1, property_bit_position));
-    CHECK_FALSE(lib::io::is_option_set(ss2, property_bit_position));
+    ss1 >> lib::io::set_option(bit_position_1);
+    CHECK(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK_FALSE(lib::io::is_option_set(ss2, bit_position_1));
 
-    ss2 >> lib::io::set_option(property_bit_position);
-    CHECK(lib::io::is_option_set(ss1, property_bit_position));
-    CHECK(lib::io::is_option_set(ss2, property_bit_position));
+    ss2 >> lib::io::set_option(bit_position_1);
+    CHECK(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK(lib::io::is_option_set(ss2, bit_position_1));
 
-    ss1 >> lib::io::unset_option(property_bit_position);
-    CHECK_FALSE(lib::io::is_option_set(ss1, property_bit_position));
-    CHECK(lib::io::is_option_set(ss2, property_bit_position));
+    ss1 >> lib::io::unset_option(bit_position_1);
+    CHECK_FALSE(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK(lib::io::is_option_set(ss2, bit_position_1));
 
-    ss2 >> lib::io::unset_option(property_bit_position);
-    CHECK_FALSE(lib::io::is_option_set(ss1, property_bit_position));
-    CHECK_FALSE(lib::io::is_option_set(ss2, property_bit_position));
+    ss2 >> lib::io::unset_option(bit_position_1);
+    CHECK_FALSE(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK_FALSE(lib::io::is_option_set(ss2, bit_position_1));
 }
 
 TEST_CASE_FIXTURE(
-    test_stream_options_manipulator, "stream_options_manipulator should properly handle ostream property operations"
+    test_stream_options_manipulator,
+    "should properly handle ostream option operations per stream"
 ) {
-    ss1 << lib::io::set_option(property_bit_position);
-    CHECK(lib::io::is_option_set(ss1, property_bit_position));
-    CHECK_FALSE(lib::io::is_option_set(ss2, property_bit_position));
+    ss1 << lib::io::set_option(bit_position_1);
+    CHECK(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK_FALSE(lib::io::is_option_set(ss2, bit_position_1));
 
-    ss2 << lib::io::set_option(property_bit_position);
-    CHECK(lib::io::is_option_set(ss1, property_bit_position));
-    CHECK(lib::io::is_option_set(ss2, property_bit_position));
+    ss2 << lib::io::set_option(bit_position_1);
+    CHECK(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK(lib::io::is_option_set(ss2, bit_position_1));
 
-    ss1 << lib::io::unset_option(property_bit_position);
-    CHECK_FALSE(lib::io::is_option_set(ss1, property_bit_position));
-    CHECK(lib::io::is_option_set(ss2, property_bit_position));
+    ss1 << lib::io::unset_option(bit_position_1);
+    CHECK_FALSE(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK(lib::io::is_option_set(ss2, bit_position_1));
 
-    ss2 << lib::io::unset_option(property_bit_position);
-    CHECK_FALSE(lib::io::is_option_set(ss1, property_bit_position));
-    CHECK_FALSE(lib::io::is_option_set(ss2, property_bit_position));
+    ss2 << lib::io::unset_option(bit_position_1);
+    CHECK_FALSE(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK_FALSE(lib::io::is_option_set(ss2, bit_position_1));
+}
+
+enum class BitPositionEnum : lib::io::bit_position_type {
+    bit_position_1 = test_stream_options_manipulator::bit_position_1,
+    bit_position_2 = test_stream_options_manipulator::bit_position_2
+};
+
+TEST_CASE_FIXTURE(
+    test_stream_options_manipulator,
+    "should properly handle istream option operations for bit position lists"
+) {
+    const auto bit_positions = {bit_position_1, bit_position_2};
+
+    ss1 >> lib::io::set_options(bit_positions);
+    CHECK(lib::io::are_option_set(ss1, bit_positions));
+    CHECK(lib::io::are_option_set(ss1, options_bitmask));
+
+    ss1 >> lib::io::unset_options(bit_positions);
+    CHECK_FALSE(lib::io::are_option_set(ss1, bit_positions));
+    CHECK_FALSE(lib::io::are_option_set(ss1, options_bitmask));
+
+    ss1 >> lib::io::set_options({bit_position_1});
+    CHECK(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK(lib::io::are_option_set(ss1, {bit_position_1}));
+    CHECK_FALSE(lib::io::is_option_set(ss1, bit_position_2));
+    CHECK_FALSE(lib::io::are_option_set(ss1, {bit_position_2}));
+    CHECK_FALSE(lib::io::are_option_set(ss1, bit_positions));
+    CHECK_FALSE(lib::io::are_option_set(ss1, options_bitmask));
+}
+
+TEST_CASE_FIXTURE(
+    test_stream_options_manipulator,
+    "should properly handle osream option operations for bit position lists"
+) {
+    const auto bit_positions = {bit_position_1, bit_position_2};
+
+    ss1 << lib::io::set_options(bit_positions);
+    CHECK(lib::io::are_option_set(ss1, bit_positions));
+    CHECK(lib::io::are_option_set(ss1, options_bitmask));
+
+    ss1 << lib::io::unset_options(bit_positions);
+    CHECK_FALSE(lib::io::are_option_set(ss1, bit_positions));
+    CHECK_FALSE(lib::io::are_option_set(ss1, options_bitmask));
+
+    ss1 << lib::io::set_options({bit_position_1});
+    CHECK(lib::io::is_option_set(ss1, bit_position_1));
+    CHECK(lib::io::are_option_set(ss1, {bit_position_1}));
+    CHECK_FALSE(lib::io::is_option_set(ss1, bit_position_2));
+    CHECK_FALSE(lib::io::are_option_set(ss1, {bit_position_2}));
+    CHECK_FALSE(lib::io::are_option_set(ss1, bit_positions));
+    CHECK_FALSE(lib::io::are_option_set(ss1, options_bitmask));
+}
+
+TEST_CASE_FIXTURE(
+    test_stream_options_manipulator,
+    "should properly handle istream option operations for enum bit position lists"
+) {
+    const auto bit_positions = {BitPositionEnum::bit_position_1, BitPositionEnum::bit_position_2};
+
+    ss1 >> lib::io::set_options(bit_positions);
+    CHECK(lib::io::are_option_set(ss1, bit_positions));
+    CHECK(lib::io::are_option_set(ss1, options_bitmask));
+
+    ss1 >> lib::io::unset_options(bit_positions);
+    CHECK_FALSE(lib::io::are_option_set(ss1, bit_positions));
+    CHECK_FALSE(lib::io::are_option_set(ss1, options_bitmask));
+
+    ss1 >> lib::io::set_options({BitPositionEnum::bit_position_1});
+    CHECK(lib::io::is_option_set(ss1, BitPositionEnum::bit_position_1));
+    CHECK(lib::io::are_option_set(ss1, {BitPositionEnum::bit_position_1}));
+    CHECK_FALSE(lib::io::is_option_set(ss1, BitPositionEnum::bit_position_2));
+    CHECK_FALSE(lib::io::are_option_set(ss1, {BitPositionEnum::bit_position_2}));
+    CHECK_FALSE(lib::io::are_option_set(ss1, bit_positions));
+    CHECK_FALSE(lib::io::are_option_set(ss1, options_bitmask));
+}
+
+TEST_CASE_FIXTURE(
+    test_stream_options_manipulator,
+    "should properly handle ostream option operations for enum bit position lists"
+) {
+    const auto bit_positions = {BitPositionEnum::bit_position_1, BitPositionEnum::bit_position_2};
+
+    ss1 << lib::io::set_options(bit_positions);
+    CHECK(lib::io::are_option_set(ss1, bit_positions));
+    CHECK(lib::io::are_option_set(ss1, options_bitmask));
+
+    ss1 << lib::io::unset_options(bit_positions);
+    CHECK_FALSE(lib::io::are_option_set(ss1, bit_positions));
+    CHECK_FALSE(lib::io::are_option_set(ss1, options_bitmask));
+
+    ss1 << lib::io::set_options({BitPositionEnum::bit_position_1});
+    CHECK(lib::io::is_option_set(ss1, BitPositionEnum::bit_position_1));
+    CHECK(lib::io::are_option_set(ss1, {BitPositionEnum::bit_position_1}));
+    CHECK_FALSE(lib::io::is_option_set(ss1, BitPositionEnum::bit_position_2));
+    CHECK_FALSE(lib::io::are_option_set(ss1, {BitPositionEnum::bit_position_2}));
+    CHECK_FALSE(lib::io::are_option_set(ss1, bit_positions));
+    CHECK_FALSE(lib::io::are_option_set(ss1, options_bitmask));
 }
 
 TEST_SUITE_END(); // test_stream_options_manipulator
