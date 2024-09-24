87d938c9d31d13252eca87c1b1ed8fd3161e535b
SpectraL519 <jmusial2003@gmail.com>
2024-09-24 15:59:02 +0200

    graph file io tests

--- tests/include/io_common.hpp
+++ tests/include/io_common.hpp
@@ -9,36 +9,40 @@
 namespace gl_testing::io_common {
 
 template <lib_tt::c_graph GraphType>
-void verify_graph_structure(const GraphType& g1, const GraphType& g2) {
-    REQUIRE_EQ(g1.n_vertices(), g2.n_vertices());
-    REQUIRE_EQ(g1.n_unique_edges(), g2.n_unique_edges());
+void verify_graph_structure(const GraphType& actual, const GraphType& expected) {
+    REQUIRE_EQ(actual.n_vertices(), expected.n_vertices());
+    REQUIRE_EQ(actual.n_unique_edges(), expected.n_unique_edges());
 
     // verify that the edges of the in graph are equivalent to the edges of the out graph
-    CHECK(std::ranges::all_of(g1.vertices(), [&](const auto& v1) {
-        return std::ranges::all_of(g1.adjacent_edges(v1), [&](const auto& edge) {
-            return g2.has_edge(edge.first_id(), edge.second_id());
+    CHECK(std::ranges::all_of(actual.vertices(), [&](const auto& v_actual) {
+        return std::ranges::all_of(actual.adjacent_edges(v_actual), [&](const auto& edge) {
+            return expected.has_edge(edge.first_id(), edge.second_id());
         });
     }));
 }
 
 template <lib_tt::c_graph GraphType>
-void verify_vertex_properties(const GraphType& g1, const GraphType& g2) {
+void verify_vertex_properties(const GraphType& actual, const GraphType& expected) {
     const auto properties_proj = [](const auto& vertex) { return vertex.properties; };
 
     CHECK(std::ranges::equal(
-        g1.vertices(), g2.vertices(), std::ranges::equal_to{}, properties_proj, properties_proj
+        actual.vertices(),
+        expected.vertices(),
+        std::ranges::equal_to{},
+        properties_proj,
+        properties_proj
     ));
 }
 
 template <lib_tt::c_graph GraphType>
-void verify_edge_properties(const GraphType& g1, const GraphType& g2) {
+void verify_edge_properties(const GraphType& actual, const GraphType& expected) {
     const auto properties_proj = [](const auto& vertex) { return vertex.properties; };
 
-    CHECK(std::ranges::all_of(g1.vertices(), [&](const auto& v1) {
-        return std::ranges::all_of(g1.adjacent_edges(v1), [&](const auto& edge) {
+    CHECK(std::ranges::all_of(actual.vertices(), [&](const auto& v_actual) {
+        return std::ranges::all_of(actual.adjacent_edges(v_actual), [&](const auto& edge) {
             // get edge returns optional<ref_wrap>
             return edge.properties
-                == g2.get_edge(edge.first_id(), edge.second_id()).value().get().properties;
+                == expected.get_edge(edge.first_id(), edge.second_id()).value().get().properties;
         });
     }));
 }
--- tests/source/test_graph_file_io.cpp
+++ tests/source/test_graph_file_io.cpp
@@ -1,66 +1,44 @@
 #include "constants.hpp"
+#include "functional.hpp"
+#include "io_common.hpp"
 
 #include <gl/graph.hpp>
-#include <gl/io.hpp>
+#include <gl/graph_file_io.hpp>
 #include <gl/topologies.hpp>
 
 #include <doctest.h>
 
-#include <sstream>
+namespace fs = std::filesystem;
 
 namespace gl_testing {
 
-TEST_SUITE_BEGIN("test_graph_io");
+#define CUSTOM_REQUIRE_THROWS_FS_ERROR(expr, errc)                                             \
+    try {                                                                                      \
+        expr;                                                                                  \
+        FAIL("Expected `std::filesystem::filesystem_error` but no exception was thrown");      \
+    }                                                                                          \
+    catch (const fs::filesystem_error& e) {                                                    \
+        const auto expected_code = std::make_error_code(errc);                                 \
+        if (e.code() != expected_code) {                                                       \
+            FAIL(std::format(                                                                  \
+                "Expected error code {}, but got {}.", expected_code.value(), e.code().value() \
+            ));                                                                                \
+        }                                                                                      \
+    }                                                                                          \
+    catch (...) {                                                                              \
+        FAIL("Expected `std::filesystem::filesystem_error` but caught a different exception"); \
+    }
+
+TEST_SUITE_BEGIN("test_graph_file_io");
 
 // Tests covering only the io functionality with the graph specification format enabled
 
-namespace common {
-
-template <lib_tt::c_graph GraphType>
-void verify_graph_structure(const GraphType& g1, const GraphType& g2) {
-    REQUIRE_EQ(g1.n_vertices(), g2.n_vertices());
-    REQUIRE_EQ(g1.n_unique_edges(), g2.n_unique_edges());
-
-    // verify that the edges of the in graph are equivalent to the edges of the out graph
-    CHECK(std::ranges::all_of(g1.vertices(), [&](const auto& v1) {
-        return std::ranges::all_of(g1.adjacent_edges(v1), [&](const auto& edge) {
-            return g2.has_edge(edge.first_id(), edge.second_id());
-        });
-    }));
-}
-
-template <lib_tt::c_graph GraphType>
-void verify_vertex_properties(const GraphType& g1, const GraphType& g2) {
-    const auto properties_proj = [](const auto& vertex) { return vertex.properties; };
-
-    CHECK(std::ranges::equal(
-        g1.vertices(), g2.vertices(), std::ranges::equal_to{}, properties_proj, properties_proj
-    ));
-}
-
-template <lib_tt::c_graph GraphType>
-void verify_edge_properties(const GraphType& g1, const GraphType& g2) {
-    const auto properties_proj = [](const auto& vertex) { return vertex.properties; };
-
-    CHECK(std::ranges::all_of(g1.vertices(), [&](const auto& v1) {
-        return std::ranges::all_of(g1.adjacent_edges(v1), [&](const auto& edge) {
-            // get edge returns optional<ref_wrap>
-            return edge.properties
-                == g2.get_edge(edge.first_id(), edge.second_id()).value().get().properties;
-        });
-    }));
-}
-
-} // namespace common
-
-struct test_directed_graph_io {
+struct test_graph_file_io {
     using traits_type =
         lib::graph_traits<lib::directed_t, lib_t::name_property, lib_t::name_property>;
     using sut_type = lib::graph<traits_type>;
 
-    test_directed_graph_io() {
-        ss << lib::io::enable_gsf;
-
+    test_graph_file_io() {
         sut_out = lib::topology::clique<sut_type>(n_vertices);
 
         // prepare vertex and edge properties
@@ -72,208 +50,84 @@ struct test_directed_graph_io {
         }
     }
 
-    const lib_t::size_type n_vertices = 5ull;
-    sut_type sut_out;
-    sut_type sut_in;
-
-    std::stringstream ss;
-};
-
-TEST_CASE_FIXTURE(
-    test_directed_graph_io, "io read should throw if the directional tag doesn't match"
-) {
-    using undir_traits = lib::graph_traits<lib::undirected_t>;
-
-    lib::graph<undir_traits> invalid_sut_out{n_vertices};
-    ss << invalid_sut_out;
-
-    CHECK_THROWS_AS(ss >> sut_in, std::ios_base::failure);
-}
-
-TEST_CASE_FIXTURE(
-    test_directed_graph_io,
-    "io read should throw for a stream with vertex properties if vertex_properties_type is not "
-    "readable"
-) {
-    ss << gl::io::with_vertex_properties << sut_out;
-
-    using not_readable_vp_traits =
-        lib::graph_traits<lib::directed_t, std::monostate, lib_t::name_property>;
-    lib::graph<not_readable_vp_traits> invalid_sut_in;
-
-    CHECK_THROWS_AS(ss >> invalid_sut_in, std::ios_base::failure);
-}
-
-TEST_CASE_FIXTURE(
-    test_directed_graph_io,
-    "io read should throw for a stream with edge properties if edge_properties_type is not readable"
-) {
-    ss << gl::io::with_edge_properties << sut_out;
-
-    using not_readable_vp_traits =
-        lib::graph_traits<lib::directed_t, lib_t::name_property, std::monostate>;
-    lib::graph<not_readable_vp_traits> invalid_sut_in;
-
-    CHECK_THROWS_AS(ss >> invalid_sut_in, std::ios_base::failure);
-}
-
-TEST_CASE_FIXTURE(
-    test_directed_graph_io,
-    "io operators should properly write and read the graph from a stream with no additional "
-    "options enabled"
-) {
-    ss << sut_out;
-    ss >> sut_in;
-
-    common::verify_graph_structure(sut_in, sut_out);
-}
-
-TEST_CASE_FIXTURE(
-    test_directed_graph_io,
-    "io operators should properly write and read the graph from a stream with vertex properties"
-) {
-    ss << lib::io::with_vertex_properties << sut_out;
-    ss >> sut_in;
-
-    common::verify_graph_structure(sut_in, sut_out);
-    common::verify_vertex_properties(sut_in, sut_out);
-}
-
-TEST_CASE_FIXTURE(
-    test_directed_graph_io,
-    "io operators should properly write and read the graph from a stream with edge properties"
-) {
-    ss << lib::io::with_edge_properties << sut_out;
-    ss >> sut_in;
-
-    common::verify_graph_structure(sut_in, sut_out);
-    common::verify_edge_properties(sut_in, sut_out);
-}
-
-TEST_CASE_FIXTURE(
-    test_directed_graph_io,
-    "io operators should properly write and read the graph from a stream with both vertex and edge "
-    "properties"
-) {
-    ss << lib::io::with_vertex_properties << lib::io::with_edge_properties << sut_out;
-    ss >> sut_in;
-
-    common::verify_graph_structure(sut_in, sut_out);
-    common::verify_vertex_properties(sut_in, sut_out);
-    common::verify_edge_properties(sut_in, sut_out);
-}
-
-struct test_undirected_graph_io {
-    using traits_type =
-        lib::graph_traits<lib::undirected_t, lib_t::name_property, lib_t::name_property>;
-    using sut_type = lib::graph<traits_type>;
-
-    test_undirected_graph_io() {
-        ss << lib::io::enable_gsf;
-
-        sut_out = lib::topology::clique<sut_type>(n_vertices);
-
-        // prepare vertex and edge properties
-        std::size_t v_idx = 0, e_idx = 0;
-        for (const auto& vertex : sut_out.vertices()) {
-            vertex.properties = {std::format("vertex_{}", v_idx++)};
-            for (const auto& edge : sut_out.adjacent_edges(vertex))
-                if (edge.first_id() == vertex.id())
-                    edge.properties = {std::format("edge_{}", e_idx++)};
-        }
+    ~test_graph_file_io() {
+        fs::remove(path);
     }
 
     const lib_t::size_type n_vertices = 5ull;
     sut_type sut_out;
-    sut_type sut_in;
 
-    std::stringstream ss;
+    fs::path path{"test_directed_graph_file_io.gsf"};
 };
 
-TEST_CASE_FIXTURE(
-    test_undirected_graph_io, "io read should throw if the directional tag doesn't match"
-) {
-    using dir_traits = lib::graph_traits<lib::directed_t>;
+TEST_CASE_TEMPLATE_DEFINE("graph file io tests", SutType, directional_tag_sut_template) {
+    using fixture_type = test_graph_file_io;
+    using sut_type = typename fixture_type::sut_type;
 
-    lib::graph<dir_traits> invalid_sut_out{n_vertices};
-    ss << invalid_sut_out;
+    fixture_type fixture;
 
-    CHECK_THROWS_AS(ss >> sut_in, std::ios_base::failure);
+    SUBCASE("save should throw in the write mode if a file already exists") {
+        std::ofstream file(fixture.path);
+        if (not file.is_open())
+            FAIL("Could not initialize an empty file");
+
+        CUSTOM_REQUIRE_THROWS_FS_ERROR(
+            lib::io::save(fixture.sut_out, fixture.path), std::errc::file_exists
+        );
+    }
+
+    SUBCASE("load shoul throw if a file does not exist") {
+        CUSTOM_REQUIRE_THROWS_FS_ERROR(
+            func::discard_result(lib::io::load<SutType>(fixture.path)),
+            std::errc::no_such_file_or_directory
+        );
+    }
+
+    // TODO: add
+    // * invalid file type tests
+    // * could not open file tests
+
+    SUBCASE("file io should properly save and load a graph in a gsf format") {
+        lib::io::save(fixture.sut_out, fixture.path);
+        const auto sut_in = lib::io::load<sut_type>(fixture.path);
+
+        io_common::verify_graph_structure(sut_in, fixture.sut_out);
+    }
+
+    SUBCASE("file io should properly save and load a graph in a gsf format with vertex properties"
+    ) {
+        lib::io::save(fixture.sut_out, fixture.path, {lib::io::with_vertex_properties});
+        const auto sut_in = lib::io::load<sut_type>(fixture.path);
+
+        io_common::verify_graph_structure(sut_in, fixture.sut_out);
+        io_common::verify_vertex_properties(sut_in, fixture.sut_out);
+    }
+
+    SUBCASE("file io should properly save and load a graph in a gsf format with edge properties") {
+        lib::io::save(fixture.sut_out, fixture.path, {lib::io::with_edge_properties});
+        const auto sut_in = lib::io::load<sut_type>(fixture.path);
+
+        io_common::verify_graph_structure(sut_in, fixture.sut_out);
+        io_common::verify_edge_properties(sut_in, fixture.sut_out);
+    }
+
+    SUBCASE("file io should properly save and load a graph in a gsf format with vertex and edge "
+            "properties") {
+        lib::io::save(fixture.sut_out, fixture.path, {lib::io::with_properties});
+        const auto sut_in = lib::io::load<sut_type>(fixture.path);
+
+        io_common::verify_graph_structure(sut_in, fixture.sut_out);
+        io_common::verify_vertex_properties(sut_in, fixture.sut_out);
+        io_common::verify_edge_properties(sut_in, fixture.sut_out);
+    }
 }
 
-TEST_CASE_FIXTURE(
-    test_undirected_graph_io,
-    "io read should throw for a stream with vertex properties if vertex_properties_type is not "
-    "readable"
-) {
-    ss << gl::io::with_vertex_properties << sut_out;
+TEST_CASE_TEMPLATE_INSTANTIATE(
+    directional_tag_sut_template,
+    lib::graph<lib::list_graph_traits<lib::directed_t>>, // directed adj list
+    lib::graph<lib::list_graph_traits<lib::undirected_t>> // undirected adj list
+);
 
-    using not_readable_vp_traits =
-        lib::graph_traits<lib::undirected_t, std::monostate, lib_t::name_property>;
-    lib::graph<not_readable_vp_traits> invalid_sut_in;
-
-    CHECK_THROWS_AS(ss >> invalid_sut_in, std::ios_base::failure);
-}
-
-TEST_CASE_FIXTURE(
-    test_undirected_graph_io,
-    "io read should throw for a stream with edge properties if edge_properties_type is not readable"
-) {
-    ss << gl::io::with_edge_properties << sut_out;
-
-    using not_readable_vp_traits =
-        lib::graph_traits<lib::undirected_t, lib_t::name_property, std::monostate>;
-    lib::graph<not_readable_vp_traits> invalid_sut_in;
-
-    CHECK_THROWS_AS(ss >> invalid_sut_in, std::ios_base::failure);
-}
-
-TEST_CASE_FIXTURE(
-    test_undirected_graph_io,
-    "io operators should properly write and read the graph from a stream with no additional "
-    "options enabled"
-) {
-    ss << sut_out;
-    ss >> sut_in;
-
-    common::verify_graph_structure(sut_in, sut_out);
-}
-
-TEST_CASE_FIXTURE(
-    test_undirected_graph_io,
-    "io operators should properly write and read the graph from a stream with vertex properties"
-) {
-    ss << lib::io::with_vertex_properties << sut_out;
-    ss >> sut_in;
-
-    common::verify_graph_structure(sut_in, sut_out);
-    common::verify_vertex_properties(sut_in, sut_out);
-}
-
-TEST_CASE_FIXTURE(
-    test_undirected_graph_io,
-    "io operators should properly write and read the graph from a stream with edge properties"
-) {
-    ss << lib::io::with_edge_properties << sut_out;
-    ss >> sut_in;
-
-    common::verify_graph_structure(sut_in, sut_out);
-    common::verify_edge_properties(sut_in, sut_out);
-}
-
-TEST_CASE_FIXTURE(
-    test_undirected_graph_io,
-    "io operators should properly write and read the graph from a stream with both vertex and edge "
-    "properties"
-) {
-    ss << lib::io::with_vertex_properties << lib::io::with_edge_properties << sut_out;
-    ss >> sut_in;
-
-    common::verify_graph_structure(sut_in, sut_out);
-    common::verify_vertex_properties(sut_in, sut_out);
-    common::verify_edge_properties(sut_in, sut_out);
-}
-
-TEST_SUITE_END(); // test_graph_io
+TEST_SUITE_END(); // test_graph_file_io
 
 } // namespace gl_testing
