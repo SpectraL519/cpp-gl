20fda6b9b25924972e7f3ee0beee8aa62bd18bb2
SpectraL519 <jmusial2003@gmail.com>
2024-09-27 11:28:42 +0200

    extracted the common verificators from test_search_algorithms to allg_common

--- tests/source/test_search_algorithms.cpp
+++ tests/source/test_search_algorithms.cpp
@@ -1,4 +1,5 @@
 #include "constants.hpp"
+#include "alg_common.hpp"
 
 #include <gl/algorithms.hpp>
 #include <gl/topologies.hpp>
@@ -8,41 +9,6 @@
 namespace gl_testing {
 
 TEST_SUITE_BEGIN("test_search_algorithms");
-
-namespace common {
-
-template <lib_tt::c_graph GraphType1, lib_tt::c_graph GraphType2>
-[[nodiscard]] auto are_vertices_equivalent(const GraphType1& g1, const GraphType2& g2) {
-    return [&](const lib_t::id_type vertex_id) {
-        const auto adj_edges_1 = g1.adjacent_edges(vertex_id);
-        const auto adj_edges_2 = g2.adjacent_edges(vertex_id);
-
-        if (adj_edges_2.distance() != adj_edges_1.distance())
-            return false;
-
-        for (const auto& edge : adj_edges_1) {
-            if (not g2.has_edge(edge.first_id(), edge.second_id()))
-                return false;
-        }
-
-        for (const auto& edge : adj_edges_2) {
-            if (not g1.has_edge(edge.first_id(), edge.second_id()))
-                return false;
-        }
-
-        return true;
-    };
-}
-
-template <lib_tt::c_instantiation_of<lib::vertex_descriptor> VertexType>
-requires(std::same_as<typename VertexType::properties_type, types::visited_property>)
-struct vertex_visited_projection {
-    [[nodiscard]] bool operator()(const VertexType& vertex) const {
-        return vertex.properties.visited;
-    }
-};
-
-} // namespace common
 
 // --- bfs tests ---
 
@@ -110,7 +76,7 @@ TEST_CASE_TEMPLATE_DEFINE(
     CHECK(std::ranges::equal(previsit_order, expected_previsit_order));
     CHECK(std::ranges::equal(postvisit_order, expected_postvisit_order));
     CHECK(std::ranges::all_of(
-        graph.vertices(), std::identity{}, common::vertex_visited_projection<vertex_type>{}
+        graph.vertices(), std::identity{}, alg_common::vertex_visited_projection<vertex_type>{}
     ));
 }
 
@@ -203,7 +169,7 @@ TEST_CASE_TEMPLATE_DEFINE(
     const auto vertex_id_view = std::views::iota(constants::first_element_idx, graph.n_vertices());
 
     // verify that for each vertex the edges are equivalent to those in the original graph
-    CHECK(std::ranges::all_of(vertex_id_view, common::are_vertices_equivalent(graph, search_tree)));
+    CHECK(std::ranges::all_of(vertex_id_view, alg_common::are_vertices_equivalent(graph, search_tree)));
 }
 
 TEST_CASE_TEMPLATE_INSTANTIATE(
@@ -286,7 +252,7 @@ TEST_CASE_TEMPLATE_DEFINE(
     CHECK(std::ranges::equal(previsit_order, expected_previsit_order));
     CHECK(std::ranges::equal(postvisit_order, expected_postvisit_order));
     CHECK(std::ranges::all_of(
-        graph.vertices(), std::identity{}, common::vertex_visited_projection<vertex_type>{}
+        graph.vertices(), std::identity{}, alg_common::vertex_visited_projection<vertex_type>{}
     ));
 }
 
@@ -379,7 +345,7 @@ TEST_CASE_TEMPLATE_DEFINE(
     const auto vertex_id_view = std::views::iota(constants::first_element_idx, graph.n_vertices());
 
     // verify that for each vertex the edges are equivalent to those in the original graph
-    CHECK(std::ranges::all_of(vertex_id_view, common::are_vertices_equivalent(graph, search_tree)));
+    CHECK(std::ranges::all_of(vertex_id_view, alg_common::are_vertices_equivalent(graph, search_tree)));
 }
 
 TEST_CASE_TEMPLATE_INSTANTIATE(
@@ -466,7 +432,7 @@ TEST_CASE_TEMPLATE_DEFINE(
     CHECK(std::ranges::equal(previsit_order, expected_previsit_order));
     CHECK(std::ranges::equal(postvisit_order, expected_postvisit_order));
     CHECK(std::ranges::all_of(
-        graph.vertices(), std::identity{}, common::vertex_visited_projection<vertex_type>{}
+        graph.vertices(), std::identity{}, alg_common::vertex_visited_projection<vertex_type>{}
     ));
 }
 
@@ -564,7 +530,7 @@ TEST_CASE_TEMPLATE_DEFINE(
     const auto vertex_id_view = std::views::iota(constants::first_element_idx, graph.n_vertices());
 
     // verify that for each vertex the edges are equivalent to those in the original graph
-    CHECK(std::ranges::all_of(vertex_id_view, common::are_vertices_equivalent(graph, search_tree)));
+    CHECK(std::ranges::all_of(vertex_id_view, alg_common::are_vertices_equivalent(graph, search_tree)));
 }
 
 TEST_CASE_TEMPLATE_INSTANTIATE(
