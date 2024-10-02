164263c76e97a24024b4bf21a5875af4a8ffcdee
SpectraL519 <jmusial2003@gmail.com>
2024-10-02 22:52:54 +0200

    resolved final comments

--- include/gl/graph_file_io.hpp
+++ include/gl/graph_file_io.hpp
@@ -102,8 +102,6 @@ void save(
     file << graph;
 }
 
-// TODO: save_as_directed
-
 template <type_traits::c_graph GraphType>
 [[nodiscard]] GraphType load(const std::filesystem::path& path = "graph.gsf") {
     std::ifstream file = detail::open_infile(path);
