51a94e9229ab94a56ea4c8fe1792a52565e3a0d3
SpectraL519 <jmusial2003@gmail.com>
2025-10-26 13:37:20 +0100

    updated licence comments

--- include/gl/algorithm/breadth_first_search.hpp
+++ include/gl/algorithm/breadth_first_search.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Jakub Musiał
+// Copyright (c) 2024-2026 Jakub Musiał
 // This file is part of the CPP-GL project (https://github.com/SpectraL519/cpp-gl).
 // Licensed under the MIT License. See the LICENSE file in the project root for full license information.
 
