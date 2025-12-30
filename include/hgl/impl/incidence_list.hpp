7468cf481c6e217624b5fbdfdc712fe11014ac2c
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2025-12-30 17:13:20 +0100

    YT-CPPHGL-15: Validate bf-directed hypergraph binding operations for the incidence list model
    Aligned the implementation of the bf-directed incidence list model to properly rebind the given vertex and hyperedge if they are already bound in a different set

--- include/hgl/impl/incidence_list.hpp
+++ include/hgl/impl/incidence_list.hpp
@@ -289,16 +289,16 @@ public:
     gl_attr_force_inline void bind_tail(
         const types::id_type vertex_id, const types::id_type hyperedge_id
     ) noexcept {
-        // TODO: validate if minor_id is not in head
         const auto [major_id, minor_id] = layout_tag::majmin(vertex_id, hyperedge_id);
+        this->_remove_no_align(this->_major_storage[major_id].head, minor_id);
         this->_unique_insert(this->_major_storage[major_id].tail, minor_id);
     }
 
     gl_attr_force_inline void bind_head(
         const types::id_type vertex_id, const types::id_type hyperedge_id
     ) noexcept {
-        // TODO: validate if minor_id is not in tail
         const auto [major_id, minor_id] = layout_tag::majmin(vertex_id, hyperedge_id);
+        this->_remove_no_align(this->_major_storage[major_id].tail, minor_id);
         this->_unique_insert(this->_major_storage[major_id].head, minor_id);
     }
 
