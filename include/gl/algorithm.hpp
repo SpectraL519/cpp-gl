259f7be7a2ba09329882d1b555855c97495cd8c3
SpectraL519 <jmusial2003@gmail.com>
2026-03-23 17:51:36 +0100

    common algorithm include

--- include/gl/algorithm.hpp
+++ include/gl/algorithm.hpp
@@ -4,9 +4,24 @@
 
 #pragma once
 
-#include "gl/algorithm/pathfinding/dijkstra.hpp"
-#include "gl/algorithm/spanning_tree/prim_mst.hpp"
-#include "gl/algorithm/topology/coloring.hpp"
-#include "gl/algorithm/topology/topological_sort.hpp"
-#include "gl/algorithm/traversal/breadth_first_search.hpp"
-#include "gl/algorithm/traversal/depth_first_search.hpp"
+// clang-format off
+
+#include "gl/algorithm/core.hpp"
+#include "gl/algorithm/traits.hpp"
+#include "gl/algorithm/util.hpp"
+
+#include "algorithm/templates/bfs.hpp"
+#include "algorithm/templates/dfs.hpp"
+#include "algorithm/templates/pfs.hpp"
+
+#include "algorithm/traversal/breadth_first_search.hpp"
+#include "algorithm/traversal/depth_first_search.hpp"
+
+#include "algorithm/topology/coloring.hpp"
+#include "algorithm/topology/topological_sort.hpp"
+
+#include "algorithm/pathfinding/dijkstra.hpp"
+
+#include "algorithm/spanning_tree/prim_mst.hpp"
+
+// clang-format on
