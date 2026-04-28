b6b2ec5a9a1699ca7fe4c8dfa7ba43b4e8ca46b1
SpectraL519 <jmusial2003@gmail.com>
2026-04-28 14:28:40 +0200

    structural tags + hypergraph traits docs

--- include/hgl/impl/impl_tags.hpp
+++ include/hgl/impl/impl_tags.hpp
@@ -2,6 +2,9 @@
 // This file is part of the CPP-GL project (https://github.com/SpectraL519/cpp-gl).
 // Licensed under the MIT License. See the LICENSE file in the project root for full license information.
 
+/// @file hgl/impl/impl_tags.hpp
+/// @brief Defines implementation tags for different hypergraph representations.
+
 #pragma once
 
 #include "hgl/decl/impl_tags.hpp"
@@ -13,46 +16,88 @@
 
 namespace hgl::impl {
 
+/// @ingroup HGL-Core
+/// @headerfile hgl/impl/impl_tags.hpp
+/// @brief Tag struct for the standard incidence list hypergraph implementation.
+/// @tparam LayoutTag Specifies the memory layout orientation for the underlying data structure.
+/// @tparam IdType The underlying integer type used for identifiers.
 template <traits::c_hypergraph_layout_tag LayoutTag, traits::c_id_type IdType>
 struct list_t {
+    /// @brief Self type alias.
     using type = list_t<LayoutTag, IdType>;
 
+    /// @brief The configured layout tag.
     using layout_tag = LayoutTag;
+    /// @brief The configured identifier type.
     using id_type = IdType;
 
+    /// @brief Type alias for the underlying incidence list hypergraph implementation based on the provided directional tag.
+    /// @tparam DirectionalTag The directional tag (e.g., undirected or bf_directed).
     template <traits::c_hypergraph_directional_tag DirectionalTag>
     using implementation_type = incidence_list<DirectionalTag, type>;
 };
 
+/// @ingroup HGL-Core
+/// @headerfile hgl/impl/impl_tags.hpp
+/// @brief Tag struct for the flattened incidence list hypergraph implementation.
+/// @tparam LayoutTag Specifies the memory layout orientation for the underlying data structure.
+/// @tparam IdType The underlying integer type used for identifiers.
+/// @see @ref gl::flat_jagged_vector "flat_jagged_vector" for the data structure used for the underlying model implementation.
 template <traits::c_hypergraph_layout_tag LayoutTag, traits::c_id_type IdType>
 struct flat_list_t {
+    /// @brief Self type alias.
     using type = flat_list_t<LayoutTag, IdType>;
 
+    /// @brief The configured layout tag.
     using layout_tag = LayoutTag;
+    /// @brief The configured identifier type.
     using id_type = IdType;
 
+    /// @brief Type alias for the underlying flattened incidence list hypergraph implementation based on the provided directional tag.
+    /// @tparam DirectionalTag The directional tag (e.g., undirected or bf_directed).
     template <traits::c_hypergraph_directional_tag DirectionalTag>
     using implementation_type = flat_incidence_list<DirectionalTag, type>;
 };
 
+/// @ingroup HGL-Core
+/// @headerfile hgl/impl/impl_tags.hpp
+/// @brief Tag struct for the standard incidence matrix hypergraph implementation.
+/// @tparam LayoutTag Specifies the memory layout orientation for the underlying data structure (must be asymmetric).
+/// @tparam IdType The underlying integer type used for identifiers.
 template <traits::c_hypergraph_asymmetric_layout_tag LayoutTag, traits::c_id_type IdType>
 struct matrix_t {
+    /// @brief Self type alias.
     using type = matrix_t<LayoutTag, IdType>;
 
+    /// @brief The configured asymmetric layout tag.
     using layout_tag = LayoutTag;
+    /// @brief The configured identifier type.
     using id_type = IdType;
 
+    /// @brief Type alias for the underlying incidence matrix hypergraph implementation based on the provided directional tag.
+    /// @tparam DirectionalTag The directional tag (e.g., undirected or bf_directed).
     template <traits::c_hypergraph_directional_tag DirectionalTag>
     using implementation_type = incidence_matrix<DirectionalTag, type>;
 };
 
+/// @ingroup HGL-Core
+/// @headerfile hgl/impl/impl_tags.hpp
+/// @brief Tag struct for the flattened incidence matrix hypergraph implementation.
+/// @tparam LayoutTag Specifies the memory layout orientation for the underlying data structure (must be asymmetric).
+/// @tparam IdType The underlying integer type used for identifiers.
+/// @see @ref gl::flat_matrix "flat_matrix" for the data structure used for the underlying model implementation.
 template <traits::c_hypergraph_asymmetric_layout_tag LayoutTag, traits::c_id_type IdType>
 struct flat_matrix_t {
+    /// @brief Self type alias.
     using type = flat_matrix_t<LayoutTag, IdType>;
 
+    /// @brief The configured asymmetric layout tag.
     using layout_tag = LayoutTag;
+    /// @brief The configured identifier type.
     using id_type = IdType;
 
+    /// @brief Type alias for the underlying flattened incidence matrix hypergraph implementation based on the provided directional tag.
+    /// @tparam DirectionalTag The directional tag (e.g., undirected or bf_directed).
     template <traits::c_hypergraph_directional_tag DirectionalTag>
     using implementation_type = flat_incidence_matrix<DirectionalTag, type>;
 };
--- include/hgl/impl/layout_tags.hpp
+++ include/hgl/impl/layout_tags.hpp
@@ -2,6 +2,9 @@
 // This file is part of the CPP-GL project (https://github.com/SpectraL519/cpp-gl).
 // Licensed under the MIT License. See the LICENSE file in the project root for full license information.
 
+/// @file hgl/impl/layout_tags.hpp
+/// @brief Defines layout tags that dictate the memory orientation and primary indexing of incidence structures.
+
 #pragma once
 
 #include "hgl/hypergraph_elements.hpp"
@@ -14,10 +17,15 @@ namespace hgl {
 
 namespace impl {
 
+/// @ingroup HGL-Core
+/// @brief Layout tag designating vertices as the primary structural dimension of the incidence representation.
 struct vertex_major_t {
+    /// @brief The major element type for this layout.
     using major_element = vertex_t;
+    /// @brief The minor element type for this layout.
     using minor_element = hyperedge_t;
 
+    /// @brief Retrieves the major element from the provided arguments based on the layout rules.
     template <std::regular T>
     [[nodiscard]] static constexpr T major(
         const T& vertex_el, [[maybe_unused]] const T& hyperedge_el
@@ -25,6 +33,7 @@ struct vertex_major_t {
         return vertex_el;
     }
 
+    /// @brief Retrieves the minor element from the provided arguments based on the layout rules.
     template <std::regular T>
     [[nodiscard]] static constexpr T minor(
         [[maybe_unused]] const T& vertex_el, const T& hyperedge_el
@@ -32,6 +41,8 @@ struct vertex_major_t {
         return hyperedge_el;
     }
 
+    /// @brief Packages data associated with the vertex and hyperedge elements into a strictly ordered major-minor pair.
+    /// @tparam T The type associated with hypergraph's elements to be ordered.
     template <std::regular T>
     [[nodiscard]] static constexpr homogeneous_pair<T> majmin(
         const T& vertex_el, const T& hyperedge_el
@@ -40,10 +51,15 @@ struct vertex_major_t {
     }
 };
 
+/// @ingroup HGL-Core
+/// @brief Layout tag designating hyperedges as the primary structural dimension of the incidence representation.
 struct hyperedge_major_t {
+    /// @brief The major element type for this layout.
     using major_element = hyperedge_t;
+    /// @brief The minor element type for this layout.
     using minor_element = vertex_t;
 
+    /// @brief Retrieves the major element from the provided arguments based on the layout rules.
     template <std::regular T>
     [[nodiscard]] static constexpr T major(
         [[maybe_unused]] const T& vertex_el, const T& hyperedge_el
@@ -51,6 +67,7 @@ struct hyperedge_major_t {
         return hyperedge_el;
     }
 
+    /// @brief Retrieves the minor element from the provided arguments based on the layout rules.
     template <std::regular T>
     [[nodiscard]] static constexpr T minor(
         const T& vertex_el, [[maybe_unused]] const T& hyperedge_el
@@ -58,6 +75,8 @@ struct hyperedge_major_t {
         return vertex_el;
     }
 
+    /// @brief Packages data associated with the vertex and hyperedge elements into a strictly ordered major-minor pair.
+    /// @tparam T The type associated with hypergraph's elements to be ordered.
     template <std::regular T>
     [[nodiscard]] static constexpr homogeneous_pair<T> majmin(
         const T& vertex_el, const T& hyperedge_el
@@ -66,25 +85,47 @@ struct hyperedge_major_t {
     }
 };
 
+/// @ingroup HGL-Core
+/// @brief Layout tag representing a symmetric or fully bidirectional incidence structural layout.
 struct bidirectional_t {};
 
 } // namespace impl
 
 namespace traits {
 
+/// @ingroup HGL-Traits
+/// @brief Validates if a type is a valid hypergraph layout tag.
+///
+/// The valid hypergraph layout tags are @ref hgl::impl::vertex_major_t "vertex_major_t",
+/// @ref hgl::impl::hyperedge_major_t "hyperedge_major_t", and @ref hgl::impl::bidirectional_t "bidirectional_t".
+///
+/// @tparam T The type to evaluate against the concept.
 template <typename T>
 concept c_hypergraph_layout_tag =
     c_one_of<T, impl::vertex_major_t, impl::hyperedge_major_t, impl::bidirectional_t>;
 
+/// @ingroup HGL-Traits
+/// @brief Validates if a type is a valid asymmetric hypergraph layout tag.
+///
+/// Symmetric or bidirectional layouts are excluded. Valid tags are @ref hgl::impl::vertex_major_t "vertex_major_t"
+/// and @ref hgl::impl::hyperedge_major_t "hyperedge_major_t".
+///
+/// @tparam T The type to evaluate against the concept.
 template <typename T>
 concept c_hypergraph_asymmetric_layout_tag =
     c_one_of<T, impl::vertex_major_t, impl::hyperedge_major_t>;
 
 } // namespace traits
 
+/// @ingroup HGL-Traits
+/// @brief Extracts the major element tag type associated with a specific asymmetric layout.
+/// @tparam LT The asymmetric layout tag type.
 template <traits::c_hypergraph_asymmetric_layout_tag LT>
 using major_element_t = typename LT::major_element;
 
+/// @ingroup HGL-Traits
+/// @brief Extracts the minor element tag type associated with a specific asymmetric layout.
+/// @tparam LT The asymmetric layout tag type.
 template <traits::c_hypergraph_asymmetric_layout_tag LT>
 using minor_element_t = typename LT::minor_element;
 
