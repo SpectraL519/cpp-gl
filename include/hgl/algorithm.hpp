805d2425c8fbeefb699a3a098a4148ad592849ad
SpectraL519 <jmusial2003@gmail.com>
2026-04-29 02:04:56 +0200

    algo umbrella header docs

--- include/hgl/algorithm.hpp
+++ include/hgl/algorithm.hpp
@@ -2,6 +2,9 @@
 // This file is part of the CPP-GL project (https://github.com/SpectraL519/cpp-gl).
 // Licensed under the MIT License. See the LICENSE file in the project root for full license information.
 
+/// @file gl/algorithm.hpp
+/// @brief Includes all algorithm-related headers, providing a comprehensive set of hypergraph algorithms for various hypergraph types and configurations.
+
 #pragma once
 
 // clang-format off
